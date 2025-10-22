f8d0bee2c2a0ead803bff70160c408ed190f2bc7
Abdullahi(busybrain) <awwalhajine@gmail.com>
2025-10-22 11:26:09 +0100

    Index and About page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,24 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: 'Home Page - GOKASUWA',
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    let viewData = [];
+    viewData['description'] = 'Purpose of the project';
+    viewData['author'] = 'Developed by: Abdullahi(Busybrain)';
+    let data1 = 'About - GOKASUWA Online Store';
+
+    return {
+      title: data1,
+      subtitle: 'About us',
+      viewData: viewData,
+    };
+  }
 }
