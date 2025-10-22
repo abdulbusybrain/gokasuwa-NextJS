a03a9f89a2feebcbeca6ff22d9f57a560cb3b98f
Abdullahi(busybrain) <awwalhajine@gmail.com>
2025-10-22 09:56:59 +0100

    Layout View

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import hbs from 'hbs';
+import hbsUtilsFactory from 'hbs-utils';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import path from 'path';
+
+const hbsUtils = hbsUtilsFactory(hbs);
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  hbs.registerPartials(path.join(__dirname, '..', 'views/layouts'));
+  hbsUtils.registerWatchedPartials(path.join(__dirname, '..', 'views/layouts'));
   app.setViewEngine('hbs');
 
   await app.listen(3000);
