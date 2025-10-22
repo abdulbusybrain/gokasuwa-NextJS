25d57e1821cad14d2cbaca2f88cccd61a8627075
Abdullahi(busybrain) <awwalhajine@gmail.com>
2025-10-22 16:42:58 +0100

    List products with Dummy Data

---
+++ src/products.controller.ts
@@ -0,0 +1,60 @@
+import { Controller, Get, Param, Render } from '@nestjs/common';
+
+@Controller('/products')
+export class ProductsController {
+  static products = [
+    {
+      id: '1',
+      name: 'Backpack',
+      description: 'Best Backpack',
+      image: 'backpack.jpg',
+      price: '1000',
+    },
+    {
+      id: '2',
+      name: 'iPhone 17',
+      description: 'Best iPhone 17',
+      image: 'iphone17.jpg',
+      price: '999',
+    },
+    {
+      id: '3',
+      name: 'Headphone',
+      description: 'Best Headphone',
+      image: 'headphone.jpg',
+      price: '30',
+    },
+    {
+      id: '4',
+      name: 'EyeGlass',
+      description: 'Best EyeGlass',
+      image: 'eyeglass.jpg',
+      price: '100',
+    },
+  ];
+
+  @Get('/')
+  @Render('products/index')
+  index() {
+    const viewData = [];
+    viewData['title'] = 'Products - GOKASUWA';
+    viewData['subtitle'] = 'List of profucts';
+    viewData['products'] = ProductsController.products;
+    return {
+      viewData: viewData,
+    };
+  }
+
+  @Get('/:id')
+  @Render('products/show')
+  show(@Param() params) {
+    const product = ProductsController.products[params.id - 1];
+    const viewData = [];
+    viewData['title'] = product.name + '- GOKASUWA';
+    viewData['subtitle'] = product.name + '- Product Information';
+    viewData['product'] = product;
+    return {
+      viewData: viewData,
+    };
+  }
+}
