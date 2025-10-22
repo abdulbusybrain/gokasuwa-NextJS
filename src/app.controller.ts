445b0fe8f444b108a0ce8519bbe19e30b31213c4
Abdullahi(busybrain) <awwalhajine@gmail.com>
2025-10-22 08:02:14 +0100

    Using Handlebars

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  // constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    // return this.appService.getHello();
-    return '<b>Hello World!</b>';
-  }
+  @Get('/')
+  @Render('index')
+  index() {}
 }
